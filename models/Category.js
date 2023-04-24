17e6a58413291fe1229db6f1f6d279298071db83
spamdalfz <coreyvasser@gmail.com>
2023-04-23 23:02:28 -0700

    set up category model

--- models/Category.js
+++ models/Category.js
@@ -4,13 +4,19 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model { }
 
+//Defines category model
 Category.init(
   {
-    // define columns
-    id: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: 
-
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
