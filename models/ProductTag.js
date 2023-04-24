4840e54c8384fc4f148cfb6f6efdf8964ee135ea
spamdalfz <coreyvasser@gmail.com>
2023-04-23 23:18:02 -0700

    defined tags model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+
   },
   {
     sequelize,
